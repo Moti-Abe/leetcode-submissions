048ad760e0103ed308cac42beeea67c908b25d36
Moti Abe <motiabe8@gmail.com>
2026-02-10 17:14:13 +0300

    Time: 13 ms (16.83%), Space: 19.5 MB (46.03%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,30 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        output = []
+
+        #iterate over character of the first word
+        for c in words[0]:
+            ok = True
+            
+            # check if c exists in all words
+            for j in range(len(words)):
+                if c not in words[j]:
+                    ok = False
+                    break
+    
+            if ok:
+                output.append(c)
+
+                # remove one occurence from each word
+                for k in range(len(words)):
+                    s = list(words[k])
+                    s.remove(c)
+                    words[k] = "".join(s)
+                
+        return output
+            
+
+               
+
+
+        
