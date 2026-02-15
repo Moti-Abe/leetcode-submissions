14de1a2fbbb4f7281c6afea4d066e12349b8651b
Moti Abe <motiabe8@gmail.com>
2026-02-15 13:14:52 +0300

    Time: 0 ms (100%), Space: 19.4 MB (20.24%) - LeetHub

--- 0789-escape-the-ghosts/0789-escape-the-ghosts.py
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -1,16 +1,16 @@
 class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-        min_ghost_dif = 1000000
+        min_ghost_distance = 1000000
         for i in range(len(ghosts)):
-            a = ghosts[i][0] - target[0]
-            b = ghosts[i][1] - target[1]
-            ghost_dif = abs(a) + abs(b)
+            x_axis_ghost_distance = ghosts[i][0] - target[0]
+            y_axis_ghost_distance = ghosts[i][1] - target[1]
+            ghost_distance = abs(x_axis_ghost_distance) + abs(y_axis_ghost_distance)
 
-            min_ghost_dif = min (min_ghost_dif, ghost_dif)
+            min_ghost_distance = min(min_ghost_distance, ghost_distance)
         
-        actual_ghost_dif = abs(0-target[0]) + abs(0-target[1])
+        player_distance = abs(0-target[0]) + abs(0-target[1])
 
-        if min_ghost_dif <= actual_ghost_dif:
+        if min_ghost_distance <= player_distance:
             return False
         else:
             return True
