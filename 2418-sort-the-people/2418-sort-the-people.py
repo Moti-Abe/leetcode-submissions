a823c054dd5b5a72a439e5e9b90a7ed2bb24e262
Moti Abe <motiabe8@gmail.com>
2026-03-03 09:38:17 +0300

    Time: 639 ms (12.95%), Space: 19.9 MB (19.4%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,19 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         mp = {}
-        output = []
         n = len(names)
         for i in range (n):
             mp[heights[i]] = names[i]
         
+        # Buble sort
+        items = list(mp.items())
+        for i in range (n):
+            for j in range (n-1-i):
+                if items[j][0] < items[j+1][0]:
+                    items[j], items[j+1] = items[j+1], items[j]
+        sorted_mp = dict(items)
         
-        sorted_mp = dict(sorted(mp.items(),reverse=True))
-        
+        output = []
         for height,name in sorted_mp.items():
             output.append(name)
         
