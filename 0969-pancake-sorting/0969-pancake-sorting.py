615a92feee5c74a6b861073793e962496854f8cb
Moti Abe <motiabe8@gmail.com>
2026-03-04 14:55:44 +0300

    Time: 0 ms (100%), Space: 19.4 MB (63.18%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -4,7 +4,6 @@ class Solution:
         fixed_value_index = len(arr)
 
         output = []
-        count = 0
         while fixed_value_index > 1:
             max_value = max(arr[:fixed_value_index])
             max_value_index = arr.index(max_value)
@@ -14,7 +13,6 @@ class Solution:
             output.append(fixed_value_index)
             arr[:fixed_value_index] = arr[:fixed_value_index][::-1]
             fixed_value_index -= 1
-            count += 2
         
         return output
 
