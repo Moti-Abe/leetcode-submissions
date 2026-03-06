1205054f0c74cdc614a099d2219de8ef716246c9
Moti Abe <motiabe8@gmail.com>
2026-03-06 10:17:26 +0300

    Time: 8 ms (25.67%), Space: 20.5 MB (58.6%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,12 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        holder , seeker = 0, 0
+        seeker , holder = 0, 0
         while seeker < len(nums):
-            if nums[seeker] != 0:
-                nums[holder], nums[seeker] =  nums[seeker], nums[holder]
+            if nums[holder] == 0 and nums[seeker] == 0:
+                seeker += 1
+            elif nums[holder] != 0 :
                 holder += 1
-            seeker += 1
+                seeker += 1
+            else:
+                nums[holder], nums[seeker] = nums[seeker], nums[holder]
+                holder += 1
+                seeker += 1
+
+        
 
 
 
