b7d4ca3caf11ebfd63d162817bda6bc766d32f83
Moti Abe <motiabe8@gmail.com>
2026-03-07 11:00:56 +0300

    Time: 0 ms (100%), Space: 19.2 MB (97.73%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,25 +4,23 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         res = []
-        left , right = 0 , 0
+        left , right = m-1 , n-1
+        res_len = len(nums1) - 1
 
-        while left < m and right < n:
-            if nums1[left] < nums2[right]:
-                res.append(nums1[left])
-                left += 1
+        while left > -1 and right > -1:
+            if nums1[left] <= nums2[right]:
+                nums1[res_len] = nums2[right]
+                res_len -= 1
+                right -= 1
             else:
-                res.append(nums2[right])
-                right += 1
+                nums1[res_len] = nums1[left]
+                res_len -= 1
+                left -= 1 
+                
+        while left == -1 and right > -1:
+            nums1[res_len] = nums2[right]
+            right -= 1
+            res_len -= 1
         
-        while left == m and right < n:
-            res.append(nums2[right])
-            right += 1
-        
-        while left < m and right == n:
-            res.append(nums1[left])
-            left += 1
-        
-        for i in range (len(res)):
-            nums1[i] = res[i]
 
         
