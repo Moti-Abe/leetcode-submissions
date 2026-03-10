243d3eed2a06a58c1bc2fa0b10925bcd25255a58
Moti Abe <motiabe8@gmail.com>
2026-03-10 14:43:50 +0300

    Time: 3 ms (44.5%), Space: 19.4 MB (53.41%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,36 +1,27 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        read = 0
+        write = 0
         n = len(chars)
 
-        st = []
-        st.append(chars[0])
-        for i in range (n):
-            if chars[i] != st[-1]:
-                st.append(chars[i])
-    
-        res = []
-        left = 0
-        for i in range (len(st)):
+        while read < n:
+            char = chars[read]
             count = 0
-            res.append(st[i])
-            
-            while left < n and st[i] == chars[left]:
-                left += 1 
-                count += 1
-            if count > 1 and count < 10:
-                res.append(str(count))
-            elif count > 9:
-                temp = list(str(count))
-                res = res + temp
 
+            while read < n and char == chars[read]:
+                read += 1
+                count += 1
+
+            chars[write] = char
+            write += 1
+            
+            if count > 1:
+                for c in str(count):
+                    chars[write] = c
+                    write += 1
+        return write
         
-        m = min(len(chars), len(res))
-        
-        for i in range (m):
-            chars[i] = res[i]
-        
-        while len(chars) > m:
-            chars.pop()
+
 
         
 
