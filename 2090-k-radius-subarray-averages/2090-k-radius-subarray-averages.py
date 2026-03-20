e0b9a7531ef1c68da24b3b1acade92b25762afcb
Moti Abe <motiabe8@gmail.com>
2026-03-20 08:56:55 +0300

    Time: 90 ms (73.62%), Space: 35.9 MB (67.78%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.py
@@ -0,0 +1,32 @@
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        total = 0
+        n = len(nums)
+        output = [-1] * n
+        if n%2 == 1 and k > (n//2):
+            return output
+        if n%2 == 0 and k > ((n//2) - 1):
+            return output
+            
+        left, right = 0, 2*k + 1
+        ind = k
+        for i in range (2*k+1):
+            total += nums[i]
+        
+        while right <= n:
+            output[ind] = total//(2*k + 1)
+            ind += 1
+            if right == n:
+                break
+            total -= nums[left]
+            left += 1
+            total += nums[right]
+            right += 1
+        
+        return output
+
+
+        
+
+        
+    
