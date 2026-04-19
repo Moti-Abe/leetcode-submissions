3afa5696e57a8476f6f42a4388294b4f13cc5ba9
Moti Abe <motiabe8@gmail.com>
2026-04-19 08:24:34 +0300

    Time: 0 ms (100%), Space: 19.2 MB (93.74%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        for i in range(numRows):
+            if i == 0:
+                res.append([1])
+            elif i == 1:
+                res.append([1,1])
+            else:
+                arr = deque()
+                for j in range (i-1):
+                    arr.append(res[i-1][j]+res[i-1][j+1])
+                arr.append(1)
+                arr.appendleft(1)
+                res.append(list(arr))
+        return res
