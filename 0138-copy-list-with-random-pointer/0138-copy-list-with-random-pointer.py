1537f531a4011dc8746efd3d15239da01ae6cced
Moti Abe <motiabe8@gmail.com>
2026-04-25 10:58:01 +0300

    Time: 46 ms (69.61%), Space: 20 MB (89.07%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -1,21 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return None
+        old_to_copy = {None:None}
 
-        old_to_new = {}
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            old_to_copy[curr] = copy
+            curr = curr.next
+        
+        curr = head 
+        while curr:
+            copy = old_to_copy[curr]
+            copy.next = old_to_copy[curr.next]
+            copy.random = old_to_copy[curr.random]
+            curr = curr.next
 
-        # 1. Create all nodes
-        cur = head
-        while cur:
-            old_to_new[cur] = Node(cur.val)
-            cur = cur.next
-
-        # 2. Assign next and random
-        cur = head
-        while cur:
-            old_to_new[cur].next = old_to_new.get(cur.next)
-            old_to_new[cur].random = old_to_new.get(cur.random)
-            cur = cur.next
-
-        return old_to_new[head]
+        return old_to_copy[head]
+        
