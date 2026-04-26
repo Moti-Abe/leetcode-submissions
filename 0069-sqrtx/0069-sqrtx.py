a09ab2c73a6abcd8e6852bb9be148b131e24246f
Moti Abe <motiabe8@gmail.com>
2026-04-26 11:15:13 +0300

    Time: 2114 ms (5.01%), Space: 21.2 MB (8.28%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -5,8 +5,14 @@ class Solution:
         for i in range (n):
             arr.append(i**2)
         
-        for i in range (n):
-            if arr[i] == x:
-                return i
-            elif arr[i] > x:
-                return i-1
+        low, high = 0, n-1
+        while low <= high:
+            mid = (low + high)//2
+            if arr[mid] < x:
+                low = mid+1
+            elif arr[mid] > x:
+                high = mid-1
+            else:
+                return mid
+        return high
+        
