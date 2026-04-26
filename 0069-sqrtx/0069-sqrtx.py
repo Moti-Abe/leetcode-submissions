cf33382bb9b239010b5beee03d20a601d934cc82
Moti Abe <motiabe8@gmail.com>
2026-04-26 11:18:10 +0300

    Time: 3 ms (66.97%), Space: 19.4 MB (8.28%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,16 +1,13 @@
 class Solution:
-    def mySqrt(self, x: int) -> int:
-        n = 50000
-        arr = []
-        for i in range (n):
-            arr.append(i**2)
-        
-        low, high = 0, n-1
+    def mySqrt(self, x: int) -> int:        
+        low, high = 0, x//2
+        if x < 2:
+            return x
         while low <= high:
             mid = (low + high)//2
-            if arr[mid] < x:
+            if mid*mid < x:
                 low = mid+1
-            elif arr[mid] > x:
+            elif mid*mid > x:
                 high = mid-1
             else:
                 return mid
