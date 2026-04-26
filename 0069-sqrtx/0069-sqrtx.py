b55f6bc8cc67b20431a2b1a39abb0952b1b5e2db
Moti Abe <motiabe8@gmail.com>
2026-04-26 11:05:16 +0300

    Time: 3248 ms (5.01%), Space: 21.1 MB (8.28%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,3 +1,12 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return(math.floor(sqrt(x)))
+        n = 50000
+        arr = []
+        for i in range (n):
+            arr.append(i**2)
+        
+        for i in range (n):
+            if arr[i] == x:
+                return i
+            elif arr[i] > x:
+                return i-1
