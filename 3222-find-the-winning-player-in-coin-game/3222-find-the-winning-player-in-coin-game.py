1d0252f65f68c704a577c0fe6d3250ed460d5ceb
Moti Abe <motiabe8@gmail.com>
2026-05-11 22:03:08 +0300

    Time: 0 ms (100%), Space: 19.3 MB (47.55%) - LeetHub

---
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def winningPlayer(self, x: int, y: int) -> str:
+        min_val = min(x, y//4)
+        if min_val%2 == 0:
+            return "Bob"
+        else:
+            return "Alice" 
