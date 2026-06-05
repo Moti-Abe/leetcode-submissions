eee72de4fd278f3ad7fdd000c49799bb91b41c8e
Moti Abe <motiabe8@gmail.com>
2026-06-05 10:00:15 +0300

    Time: 1805 ms (5.35%), Space: 19.3 MB (74.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = list(s)
-        s.sort()
-        t = list(t)
-        t.sort()
-        return s == t
+        if len(s) != len(t):
+            return False
+        smap, tmap = {}, {}
+        for c in s:
+            smap[c] = s.count(c)
+        for c in t:
+            tmap[c] = t.count(c)
         
+        for key,value in smap.items():
+            if key not in tmap or tmap[key] != value:
+                return False
+        return True
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
