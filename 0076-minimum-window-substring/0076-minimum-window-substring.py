31c2d1ff8ea3b9d586c6b24bc11b965d9d1d80d7
Moti Abe <motiabe8@gmail.com>
2026-06-17 10:55:41 +0300

    Time: 531 ms (8.45%), Space: 19.6 MB (73.7%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,44 +2,35 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(s) < len(t):
             return ""
+        tmpp, smpp = {}, {}
+        res = ""
+        res_len = len(s)
+        for c in t:
+            tmpp[c] = tmpp.get(c,0) + 1
+        def isTrue():
+            for key, val in tmpp.items():
+                if smpp.get(key, 0) < val:
+                    return False
+            return True
+
+        l, r = 0, 0
         
-        left, right, n = 0, 0, len(s)
-        tmp, smp = {}, {}
-        min_len = float('inf')
-        ind = -1
+        while r < len(s):
+            if s[r] in  t:
+                smpp[s[r]] = smpp.get(s[r], 0) + 1
+            while len(tmpp) == len(smpp) and isTrue():
+                 
+                if r-l+1 <= res_len:
+                    res = s[l:r+1]
+                    res_len = r-l+1
+                if s[l] in smpp:
+                    smpp[s[l]] -= 1
+                if s[l] in smpp and smpp[s[l]] == 0:
+                    del smpp[s[l]]
+                l += 1
+            r += 1
+        return res
 
-        for i in range (len(t)):
-            tmp[t[i]] = tmp.get(t[i], 0) + 1
-        
-        def isvalid():
-                for c in tmp:
-                    if tmp[c] > smp.get(c, 0):
-                        return False
-                return True
-
-        while right < n:
-            smp[s[right]] = smp.get(s[right], 0) + 1
-            
-            while isvalid():
-                if (right - left + 1) <= min_len:
-                    ind = left
-                    min_len = right - left + 1
-                smp[s[left]] -= 1
-                left += 1
-            right += 1
-        
-        return "" if ind == -1 else s[ind: ind + min_len]
-                
-            
-
-
-        
-
-
-
- 
-
-        
-
-
-                
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
