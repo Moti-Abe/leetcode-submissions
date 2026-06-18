c2ffdfb1ce0f5761261e23d623ad54597fbb40aa
Moti Abe <motiabe8@gmail.com>
2026-06-18 10:13:48 +0300

    Time: 3 ms (34.05%), Space: 19.1 MB (99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = deque()
+        stack = []
         for i in range (len(s)):
-            if s[i] == '(' or s[i] == '[' or s[i] == '{':
-                  stack.append(s[i])
+            if s[i] == '{' or s[i] == '{' or s[i] == '(' or s[i] == '[':
+                stack.append(s[i])
             else:
                 if stack:
-                    if (s[i] == ')' and stack[-1] == '(') or (s[i] == ']' and stack[-1] == '[') or (s[i] == '}' and stack[-1] == '{'):
+                    if stack[-1] == '{' and s[i] == '}' or stack[-1] == '[' and s[i] == ']' or stack[-1] == '(' and s[i] == ')':
                         stack.pop()
                     else:
                         return False
                 else:
                     return False
             
-        if  stack:
-            return False
-        else:
+        if not stack:
             return True
-                
+        else:
+            return False
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
