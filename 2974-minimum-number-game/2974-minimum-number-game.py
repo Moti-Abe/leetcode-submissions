ab465cbfe2a4cb0354106ad8cdeb9cfb8658a69a
Moti Abe <motiabe8@gmail.com>
2026-07-11 15:48:26 +0300

    Time: 0 ms (100%), Space: 19.2 MB (59.61%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        for i in range (0,len(nums)-1,2):
+            temp = nums[i]
+            nums[i] = nums[i+1]
+            nums[i+1] = temp
+        return nums
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
