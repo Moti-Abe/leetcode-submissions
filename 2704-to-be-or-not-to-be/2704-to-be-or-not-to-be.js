a1b349f6f2f8bbddf2ac48ae16666aa885afbb97
Moti Abe <motiabe8@gmail.com>
2026-08-06 08:58:01 +0300

    Time: 55 ms (5.4%), Space: 54 MB (25.23%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    var toBe = function(other){
+        if  (val === other){
+            return true
+        }else{
+            throw new Error("Not Equal")
+        }
+    }
+    var notToBe = function(other){
+        if  (val !== other){
+            return true
+        }else{
+            throw new Error("Equal")
+        }
+    }
+    return{
+        toBe,
+        notToBe
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
