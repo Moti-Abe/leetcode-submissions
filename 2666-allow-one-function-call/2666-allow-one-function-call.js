1512f9c5ac57569081271213e77a4db4820f3ef3
Moti Abe <motiabe8@gmail.com>
2026-08-07 14:57:09 +0300

    Time: 47 ms (38.25%), Space: 51.5 MB (99.63%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    
+    let count = 0
+    return function (...args) {
+        count += 1
+        if (count > 1){
+            return undefined
+        }
+        return fn(...args)
+    };
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
