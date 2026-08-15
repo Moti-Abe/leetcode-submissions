9c21b950f2d655b19c0ee7aeaecd603121e855dc
Moti Abe <motiabe8@gmail.com>
2026-08-15 21:43:48 +0300

    Time: 47 ms (66.23%), Space: 55.3 MB (31.15%) - LeetHub

---
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.ts
@@ -0,0 +1,51 @@
+class TimeLimitedCache {
+    private map = new Map<number, {value: number, expires: number}>();
+    
+    set(key: number, value: number, duration: number): boolean {
+        const exists = this.map.has(key)
+        
+        this.map.set(key,{
+            value:value,
+            expires: Date.now()+ duration
+        });
+        return exists
+    }
+    
+    get(key: number): number {
+        const item =  this.map.get(key)
+
+        if (!item) return -1
+
+        if (Date.now() >= item.expires) {
+            this.map.delete(key);
+            return -1;
+        }
+
+        return item.value
+    }
+    
+    count(): number {
+        let count = 0;
+
+        for (const [key, item] of this.map) {
+            if (Date.now() < item.expires) {
+                count++;
+            } else {
+                this.map.delete(key);
+            }
+        }
+
+        return count;
+    }
+}
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
