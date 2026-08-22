4b14924dbd004df0811502ede2f54f58517c18b4
Moti Abe <motiabe8@gmail.com>
2026-08-22 13:50:19 +0300

    Time: 45 ms (79.76%), Space: 52.7 MB (97.72%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    
+    let ref
+    return function(...args) {
+        clearTimeout(ref)
+        
+        ref = setTimeout(()=>{
+            return fn(...args)
+        }, t)
+
+        
+    
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
