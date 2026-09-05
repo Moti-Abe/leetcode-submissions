863e5df0380530c03ffe6ba77e625f5e8e8173f4
Moti Abe <motiabe8@gmail.com>
2026-09-05 22:15:31 +0300

    Time: 7 ms (27.96%), Space: 19.5 MB (10.35%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,33 +1,36 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range (9):
+        row = len(board)
+        col = len(board[0])
+        
+        for i in range(row):
             arr1 = []
             arr2 = []
-            for j in range (9):
+            for j in range(col):
                 if board[i][j].isdigit():
                     arr1.append(board[i][j])
                 
                 if board[j][i].isdigit():
                     arr2.append(board[j][i])
-
+            
             if len(arr1) != len(set(arr1)):
                 return False
             if len(arr2) != len(set(arr2)):
                 return False
 
-        for r in range(0,9,3):
-            for c in range(0,9,3):
-                arr = []
+        for r in range(0, 9, 3):
+            for c in range(0, 9, 3):
+                box = []
                 for i in range(3):
                     for j in range(3):
-                        if board[r+i][c+j].isdigit():
-                            arr.append(board[r+i][c+j])
-                
-                
-                if len(arr) != len(set(arr)):
+                        val = board[r + i][c + j]
+                        if val.isdigit():
+                            box.append(val)
+                if len(box) != len(set(box)):
                     return False
-        
+
         return True
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
