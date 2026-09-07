554bc70907845abc4bcbe875f684848be630cb90
Moti Abe <motiabe8@gmail.com>
2026-09-07 10:21:09 +0300

    Time: 13 ms (27.63%), Space: 21.1 MB (25.14%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -8,11 +8,20 @@ class Solution:
         suffix_max = height.copy()
         for i in range (n-2,-1,-1):
             suffix_max[i] = max(suffix_max[i+1], height[i])
-        
+
         total = 0
-        for i in range (n):
-            left_max, right_max = prefix_max[i], suffix_max[i]
+        for i in range(len(height)):
+            left_max = prefix_max[i]
+            right_max = suffix_max[i]
+
             if height[i] < left_max and height[i] < right_max:
-                total += (min(left_max, right_max) - height[i])
+                total += min(left_max, right_max) - height[i]
         
         return total
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
