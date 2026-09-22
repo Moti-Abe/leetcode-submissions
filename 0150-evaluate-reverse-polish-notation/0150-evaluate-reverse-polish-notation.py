ff23a673df1db2dc51584afe7ada4454ead98603
Moti Abe <motiabe8@gmail.com>
2026-09-22 10:48:42 +0300

    Time: 90 ms (6.4%), Space: 20.6 MB (74.81%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,24 +1,16 @@
 class Solution:
-    def evalRPN(self, tokens: list[str]) -> int:
-        st = deque()
-        for i in range(len(tokens)):
-            if tokens[i] not in ["+", "-", "*", "/"]:
-                st.append(int(tokens[i]))
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = deque()
+        for i in range (len(tokens)):
+            if tokens[i] == '+' or tokens[i] == '-' or tokens[i] == '*' or tokens[i] == '/':
+                x = stack.pop()
+                y = stack.pop()
+                if tokens[i] != '/':
+                    res = eval(f"{y} {tokens[i]} {x}")
+                else:
+                    res = int(y/x)
+                stack.append(res)
             else:
-                if len(st) >= 2:
-                    x = st.pop()
-                    y = st.pop()
-                    if tokens[i] == "+":
-                        res = y + x
-                    elif tokens[i] == "-":
-                        res = y - x
-                    elif tokens[i] == "*":
-                        res = y * x
-                    else:
-                        res = int(y / x)
-                    st.append(res) 
-        return st[0]
-
-# Synced seamlessly with LeetHub Pro
-# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+                stack.append(int(tokens[i]))
+        
+        return stack[0]
