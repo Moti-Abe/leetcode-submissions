c8e05b2bc8622966f391c797d505b5ebaee01aa6
Moti Abe <motiabe8@gmail.com>
2026-09-22 09:58:20 +0300

    Time: 6 ms (100%), Space: 23 MB (100%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,43 +1,35 @@
 class MinStack:
 
     def __init__(self):
-        self.st = deque()
-        
-    def push(self, value: int) -> None:
-        self.val = value
-        
-        if not self.st:
-            self.st.append((value, self.val))
+        self.stack = deque()
+        self.val = 0
+
+    def push(self, val: int) -> None:
+        self.val = val
+        if not self.stack:
+            self.stack.append((self.val, self.val))
         else:
-            x = self.st[-1][1]
-            if x >= self.val:
-                self.st.append((value, self.val))
+            x = self.stack[-1][1]
+            if self.val <= x:
+                self.stack.append((self.val, self.val))
             else:
-                self.st.append((value, x))
-        
+                self.stack.append((self.val, x))
 
     def pop(self) -> None:
-        if self.st: 
-            self.st.pop()
-        
+        if self.stack:
+            self.stack.pop()
 
     def top(self) -> int:
-        if self.st: 
-            return self.st[-1][0]
-        
+        if self.stack:
+            return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.st[-1][1]
-        
+        return self.stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
-# obj.push(value)
+# obj.push(val)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
-
-# Synced seamlessly with LeetHub Pro
-# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
